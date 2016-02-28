7de543d77460e1f0a83e2406020a438c22de2e90
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-02-29 00:21:06 +0530

    Ajaxifying tasks completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,16 @@
 class TasksController < ApplicationController
   def index
-    @not_started_tasks = Task.where(status: Task::Status::NOT_STARTED)
-    @in_progress_tasks = Task.where(status: Task::Status::IN_PROGRESS)
-    @finished_tasks = Task.where(status: Task::Status::FINISHED)
+    @not_started_tasks = Task.where(status:
+     Task::Status::NOT_STARTED)
+    @in_progress_tasks = Task.where(status:
+     Task::Status::IN_PROGRESS)
+    @finished_tasks = Task.where(status:
+     Task::Status::FINISHED)
   end
 
   def update
     @task = Task.find(params[:id])
     @task.update_attribute(:status,
     params[:status].to_i)
-    redirect_to tasks_path
   end
 end
