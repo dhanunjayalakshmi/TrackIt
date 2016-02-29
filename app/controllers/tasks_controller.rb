56de87fffff661c4582b23118e55994d9abb8410
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-02-29 14:35:59 +0530

    Install Devise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,7 @@ class TasksController < ApplicationController
      Task::Status::IN_PROGRESS)
     @finished_tasks = Task.where(status:
      Task::Status::FINISHED)
+     flash.now[:error] = "Error!!!"
   end
 
   def new
