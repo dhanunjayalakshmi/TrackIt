59b00f43a1c728fd0ed570418d5b7e1099b261db
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-03-02 19:35:32 +0530

    Refactor: Share Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :tasks, except: [:show]
-  resources :task_shares, only: [:new, :create]
+  resources :tasks, except: [:show] do
+    resources :task_shares, only: [:new, :create]
+  end
   # You can have the root of your site routed with "root"
   root 'home#land'
   # Example of regular route:
